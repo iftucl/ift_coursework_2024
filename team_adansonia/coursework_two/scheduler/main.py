bb8e3384512119ccd54e980a33c5761a99c2d662
phumpakorn s <phumpakorns@gmail.com>
2025-05-02 19:57:34 +0100

    scheduling

---
+++ team_adansonia/coursework_two/scheduler/main.py
@@ -0,0 +1,19 @@
+import sys
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+# Add parent directory to import path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from team_adansonia.scheduler_test import say_hello  # now this works!
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+    scheduler.add_job(say_hello, CronTrigger(minute="*"))  # Run every minute
+
+    try:
+        print("Scheduler started. Press Ctrl+C to exit.")
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        print("Shutting down scheduler...")
